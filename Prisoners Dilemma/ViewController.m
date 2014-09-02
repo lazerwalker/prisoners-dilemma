eb84ba814740cfb84ae0f045d912bf407f862c40
Mike Walker <michael@lazerwalker.com>
2014-09-01 22:55:22 -0400

    Show a MCBrowserViewController

--- Prisoners Dilemma/ViewController.m
+++ Prisoners Dilemma/ViewController.m
@@ -1,27 +1,56 @@
-//
-//  ViewController.m
-//  Prisoners Dilemma
-//
-//  Created by Michael Walker on 8/31/14.
-//  Copyright (c) 2014 Mike Lazer-Walker. All rights reserved.
-//
+@import MultipeerConnectivity;
 
 #import "ViewController.h"
 
-@interface ViewController ()
+static NSString * const ServiceType = @"mlw-prisoner";
 
+@interface ViewController ()<MCBrowserViewControllerDelegate, MCAdvertiserAssistantDelegate, MCSessionDelegate>
+
+@property (readwrite, nonatomic, strong) MCSession *session;
+@property (readwrite, nonatomic, strong) MCAdvertiserAssistant *assistant;
 @end
 
 @implementation ViewController
-            
+
 - (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    UIDevice *device = [UIDevice currentDevice];
+    MCPeerID *peer = [[MCPeerID alloc] initWithDisplayName:device.name];
+    self.session = [[MCSession alloc] initWithPeer:peer];
+    self.session.delegate = self;
+
+    self.assistant = [[MCAdvertiserAssistant alloc] initWithServiceType:ServiceType
+                                                          discoveryInfo:nil
+                                                                session:self.session];
+    self.assistant.delegate = self;
+    [self.assistant start];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+
+    if (self.session.connectedPeers.count == 0) {
+        MCBrowserViewController *browser = [[MCBrowserViewController alloc] initWithServiceType:@"mlw-prisoner" session:self.session];
+        browser.delegate = self;
+        [self presentViewController:browser animated:YES completion:nil];
+    }
+}
+
+#pragma mark - MCBrowserViewControllerDelegate
+
+- (void)browserViewControllerDidFinish:(MCBrowserViewController *)browserViewController {
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+- (void)browserViewControllerWasCancelled:(MCBrowserViewController *)browserViewController {
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+#pragma mark - MCSessionDelegate
+
+- (void)session:(MCSession *)session peer:(MCPeerID *)peerID didChangeState:(MCSessionState)state {
+    if (state == MCSessionStateConnected) {
+        [self dismissViewControllerAnimated:YES completion:nil];
+    }
 }
 
 @end
