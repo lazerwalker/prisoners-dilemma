aaed5b9ef5694d66e8cd9b480f4a36866f52139d
Mike Walker <michael@lazerwalker.com>
2014-09-04 12:11:56 -0400

    Remove header boilerplate

--- Prisoners Dilemma/AppDelegate.m
+++ Prisoners Dilemma/AppDelegate.m
@@ -1,11 +1,3 @@
-//
-//  AppDelegate.m
-//  Prisoners Dilemma
-//
-//  Created by Michael Walker on 8/31/14.
-//  Copyright (c) 2014 Mike Lazer-Walker. All rights reserved.
-//
-
 #import "AppDelegate.h"
 
 @interface AppDelegate ()
--- Prisoners Dilemma/ViewController.h
+++ Prisoners Dilemma/ViewController.h
@@ -1,15 +1,4 @@
-//
-//  ViewController.h
-//  Prisoners Dilemma
-//
-//  Created by Michael Walker on 8/31/14.
-//  Copyright (c) 2014 Mike Lazer-Walker. All rights reserved.
-//
-
-#import <UIKit/UIKit.h>
-
 @interface ViewController : UIViewController
-
 
 @end
 
